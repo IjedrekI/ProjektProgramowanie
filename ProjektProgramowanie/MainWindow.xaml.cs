c090414234cba92f5cbb7ac9c9a3c3e8634b0eca
Andrzej Witas <andrzej.witas@capgemini.com>
2019-02-03 08:07:21 +0100

    add validation

--- ProjektProgramowanie/MainWindow.xaml.cs
+++ ProjektProgramowanie/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -56,5 +57,12 @@ namespace ProjektProgramowanie
             context.SaveChanges();
 
         }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
     }
+
 }
