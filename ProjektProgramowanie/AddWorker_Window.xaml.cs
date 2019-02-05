ec2f80f9281a727238643c8d26f91d8f2894aee8
Lukasz <lukaszu@hotmail.com>
2019-02-05 18:37:57 +0100

    added 'workers' window and 'add worker window'

---
+++ ProjektProgramowanie/AddWorker_Window.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ProjektProgramowanie.Infrastructure;
+using ProjektProgramowanie.Infrastructure.Entities;
+using System.Linq;
+using System.Windows;
+
+
+namespace ProjektProgramowanie
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddWorker_Window.xaml
+    /// </summary>
+    public partial class AddWorker_Window : Window
+    {
+        public AddWorker_Window()
+        {
+            InitializeComponent();
+        }
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var context = new ToDoContext();
+
+            Worker worker = new Worker()
+            {
+                Name = nameInput.Text
+            };
+
+            context.Workers.Add(worker);
+            context.SaveChanges();
+            Workers_Window.dataGrid.ItemsSource = context.Workers.ToList();
+            //MainWindow.dataGrid.ItemsSource = context.ToDoItems.ToList();
+            Hide();
+        }
+    }
+}
---
+++ ProjektProgramowanie/Workers_Window.xaml.cs
@@ -0,0 +1,61 @@
+﻿using ProjektProgramowanie.Infrastructure;
+using ProjektProgramowanie.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjektProgramowanie
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Workers_Window.xaml
+    /// </summary>
+    public partial class Workers_Window : Window
+    {
+        ToDoContext context = new ToDoContext();
+        public static DataGrid dataGrid;
+
+        public Workers_Window()
+        {
+            InitializeComponent();
+            LoadDbData();
+        }
+
+        private void LoadDbData()
+        {
+            xamlDataGrid.ItemsSource = context.Workers.ToList();
+            dataGrid = xamlDataGrid;
+        }
+
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddWorker_Window aww = new AddWorker_Window();
+            aww.ShowDialog();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (xamlDataGrid.SelectedItem as Worker).Id;
+            //EditToDoItemWindow editToDoItemWindow = new EditToDoItemWindow(id);
+            //editToDoItemWindow.ShowDialog();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (xamlDataGrid.SelectedItem as Worker).Id;
+            var deleteItem = context.Workers.Where(item => item.Id == id).Single();
+            context.Workers.Remove(deleteItem);
+            context.SaveChanges();
+            xamlDataGrid.ItemsSource = context.Workers.ToList();
+        }
+    }
+}
