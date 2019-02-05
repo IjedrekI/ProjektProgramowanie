3b19d5d25ee34c9bc50f4e2724349d8a4c5d9b7d
Lukasz <lukaszu@hotmail.com>
2019-02-05 18:29:18 +0100

    added main window

--- ProjektProgramowanie/MainWindow.xaml.cs
+++ ProjektProgramowanie/MainWindow.xaml.cs
@@ -24,45 +24,46 @@ namespace ProjektProgramowanie
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ToDoContext db = new ToDoContext();
+        public static DataGrid dataGrid;
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadDbData();
         }
-        private void Send_Click(object sender, RoutedEventArgs e)
+        private void LoadDbData()
         {
-            var context = new ToDoContext();
-
-            var shopInString = shopTextBox.Text;
-            Enum.TryParse(shopInString, out Shop shop);
-
-            Worker worker = new Worker()
-            {
-                Name = "Stefan"
-            };
-
-            ToDoItem item = new ToDoItem()
-            {
-                Name = nameTextBox.Text,
-                Shop = shop,
-                Quantity = int.Parse(quantityBox.Text),
-                //Notes = notesTextBox.Text,
-                //Date = datenotesTextBox.Text
-                WorkerId = worker.Id
-
-            };
-
-
-            context.Workers.Add(worker);
-            context.ToDoItems.Add(item);
-            context.SaveChanges();
-
+            xamlDataGrid.ItemsSource = db.ToDoItems.ToList();
+            dataGrid = xamlDataGrid;
         }
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        private void WorkerBtn_Click(object sender, RoutedEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
+            Workers_Window ww = new Workers_Window();
+            ww.Show();
         }
 
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddTask_Window atw = new AddTask_Window();
+            atw.ShowDialog();
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //int id = (xamlDataGrid.SelectedItem as ToDoItem).Id;
+            //EditToDoItemWindow editToDoItemWindow = new EditToDoItemWindow(id);
+            //editToDoItemWindow.ShowDialog();
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (xamlDataGrid.SelectedItem as ToDoItem).Id;
+            var deleteItem = db.ToDoItems.Where(item => item.Id == id).Single();
+            db.ToDoItems.Remove(deleteItem);
+            db.SaveChanges();
+            xamlDataGrid.ItemsSource = db.ToDoItems.ToList();
+        }
     }
-
 }
