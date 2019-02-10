eada888bd524c4b1744dcab1c8a2b70a9807004e
Andrzej Witas <andrzej.witas@capgemini.com>
2019-02-10 20:38:49 +0100

    Finalls

---
+++ Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CheckIfWorkerExist()
+        {
+            Worker worker = new Infrastructure.Entities.Worker();
+        }
+        
+    }
+}
